bf2dd3bd950d1691eb7614170c84c5f52d77aab8
YH Yang <yhuiyang@gmail.com>
2014-05-08 15:55:15 +0800

    Initial import project.
    The project files are generated by tool, gdx-setup.jar.

---
+++ android/src/com/yhsoftlab/gdxtest/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.yhsoftlab.gdxtest.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.yhsoftlab.gdxtest.MyGdxTestGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxTestGame(), config);
+	}
+}
---
+++ desktop/src/com/yhsoftlab/gdxtest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yhsoftlab.gdxtest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yhsoftlab.gdxtest.MyGdxTestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxTestGame(), config);
+	}
+}
