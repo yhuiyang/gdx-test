3fa234ae31639a52e1db9e8f91e275c8b6141c71
YH Yang <yhuiyang@gmail.com>
2014-05-09 15:54:53 +0800

    Show bitmapfont that generated by freetype.

--- core/src/com/yhsoftlab/gdxtest/MyGdxTestGame.java
+++ core/src/com/yhsoftlab/gdxtest/MyGdxTestGame.java
@@ -4,16 +4,27 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class MyGdxTestGame extends ApplicationAdapter {
     SpriteBatch batch;
-    Texture img;
+    BitmapFont font;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+
+        FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("WCL-10.ttf"));
+        gen.setMaxTextureSize(512);
+        FreeTypeFontParameter fontParam = new FreeTypeFontParameter();
+        fontParam.size = 38;
+        fontParam.characters = "ABCDE Libgdx事先預備好的中文！等一下來使用，沒有就是看不見。";
+        fontParam.flip = false;
+        font = gen.generateFont(fontParam);
+        gen.dispose();
     }
 
     @Override
@@ -21,7 +32,14 @@ public class MyGdxTestGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0);
+        //batch.draw(font.getRegion(), 0, 0);
+        font.draw(batch, "Libgdx是可以使用中文的！", 50, 350);
         batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        font.dispose();
     }
 }
